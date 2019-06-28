ca8f0fa244f2166de4cad3dba220a8768bba6f4a
youngqiankun <youngqiankun@163.com>
2019-06-28 10:45:00 +0800

    Initial Commit

---
+++ cpp-basic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp-basic
+//
+//  Created by Mr.Yang on 6/28/19.
+//  Copyright © 2019 Mr.Yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
